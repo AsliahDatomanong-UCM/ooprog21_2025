1e8e145d9c2103ac7c621a759385afe07bd8d5b5
AsliahDatomanong-UCM <datumanongasliah@gmail.com>
2025-09-17 20:34:17 +0800

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,21 @@
+public class Comments {
+    public static void main(String[] args) {
+        
+        // ---- Single-line comment ----
+        // This line prints a message to the console
+        System.out.println("Hello, this is a single-line comment example.");
+        
+        /* ---- Multi-line comment ----
+           The next line of code will print another message.
+           Multi-line comments are useful when writing longer explanations.
+        */
+        System.out.println("This is a multi-line comment example.");
+        
+        /**
+         * ---- Documentation comment ----
+         * The following line of code prints a final message.
+         * Documentation comments can be extracted into HTML docs using Javadoc.
+         */
+        System.out.println("This is a documentation comment example.");
+    }
+}
---
+++ FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("I'm in a reckless fever");
+        System.out.println("Love-struck girl, I'd tease her");
+        System.out.println("Thought I'd never be her");
+        System.out.println("Quite the job you've done on me, sir");
+    }
+}
