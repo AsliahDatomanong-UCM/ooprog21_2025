d6a520a6224c1eb380a7903561e14f9fb8974eff
AsliahDatomanong-UCM <datumanongasliah@gmail.com>
2025-09-22 22:17:10 +0800

    Add files via upload

---
+++ Chapter_2/CompanyInfo.java
@@ -0,0 +1,14 @@
+public class CompanyInfo {
+
+    public static void displayHours() {
+        System.out.println("Monday - Friday  8:00 AM to 6:00 PM");
+        System.out.println("Saturday         8:00 AM to Noon");
+        System.out.println("Sunday           Closed");
+        
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Smart Electronics");
+        displayHours();
+    }
+}
